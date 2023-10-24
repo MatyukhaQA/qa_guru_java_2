09b322ced493cba6ef8df5e6126822c12ce331f6
masha <m@matyuha.ru>
2023-10-24 13:18:38 +0300

    refactoring main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
+    private static String msg = "Hello world!";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(msg);
     }
 }
